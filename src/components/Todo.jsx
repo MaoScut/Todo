f873a003810794a9daf64074696a7cd7b756f954
MaoScut <849345372@qq.com>
2017-05-11 15:27:43 +0800

    finish basic function, but I haven't understand details!

---
+++ src/components/Todo.jsx
@@ -0,0 +1,55 @@
+import TodoAction from '../../actions/TodoAction';
+import uuid from 'uuid';
+import React from 'react';
+import TodoStore from '../../stores/TodoStore.js';
+
+class Todo extends React.Component {
+	constructor(props){
+		super(props);
+		this.state={
+			todos: TodoStore.getAll()
+		};
+		this.createTodo = this.createTodo.bind(this);
+		this.deleteTodo = this.deleteTodo.bind(this);
+		this.onChange = this.onChange.bind(this);
+	}
+	componentDidMount(){
+		TodoStore.addChangeListener(this.onChange);
+	}
+	componentWillUnmount(){
+		TodoStore.removeChangeListListener(this.onChange);
+	}
+	createTodo(){
+		TodoAction.create({id:uuid.v4(),content: '3rd stuff'});
+	}
+	onChange(){
+		this.setState({
+			todos: TodoStore.getAll()
+		})
+	}
+	deleteTodo(id){
+		TodoAction.delete(id);
+	}
+	render(){
+		return(
+			<div>
+				<List items={this.state.todos} onDelete={this.deleteTodo}></List>
+				<button onClick={this.createTodo}>createTodo</button>
+			</div>
+			)
+	}
+}
+
+class List extends React.Component {
+	render(){
+		//debugger;
+		var list = this.props.items.map(value=>(<li><button onClick={()=>this.props.onDelete(value.id)}>delete</button>{value.content}</li>));
+		return (
+			<ul>
+				{list}
+			</ul>
+			)
+	}
+}
+
+export default Todo;
